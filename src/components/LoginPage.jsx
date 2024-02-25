270c338faaa80ae8ead98335e8884e8a88f07273
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2024-02-25 19:26:47 +0530

    client for fb-dm-helpdesk

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {LoginCard} from "./LoginCard";
+
+export const LoginPage = () => {
+  return (
+    <div className = "bg-blue-900 min-h-screen min-w-screen flex items-center justify-center">
+        <LoginCard/>
+    </div>
+  )
+}
+
---
+++ src/components/SignUpPage.jsx
@@ -0,0 +1,9 @@
+import { SignUpCard } from "./SignUpCard";
+
+export const SignUpPage = () => {
+  return (
+    <div className = "bg-blue-900 min-h-screen min-w-screen flex items-center justify-center">
+      <SignUpCard/>
+    </div>
+  );
+}
